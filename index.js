753e1e0e2395421ccc0744abd1a451bfbe801045
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-10 11:23:17 +0530

    Added ENV

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.send("<h2>Hello World</h2>");
+  res.send("<h2>Hello World!!</h2>");
 });
 
 const PORT = process.env.PORT || 3000;
