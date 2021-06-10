980d28217927934923afddbdbd69613dfc1589e9
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-10 09:26:19 +0530

    Wrote Dockerfile instructions

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("<h2>Hello World</h2>");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
